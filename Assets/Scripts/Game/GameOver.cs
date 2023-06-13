82e691120746183984d4a2e83947d52ddb06eabf
Andrew <reotmg@gmail.com>
2023-06-13 16:50:15 +0300

    updated Scripts

--- Assets/Scripts/Game/GameOver.cs
+++ Assets/Scripts/Game/GameOver.cs
@@ -7,7 +7,6 @@ public class GameOver : MonoBehaviour
 {
     private Button _button;
     private bool _finished = false;
-    // Start is called before the first frame update
     void Start()
     {
         _button = GetComponent<Button>();
@@ -15,7 +14,6 @@ public class GameOver : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (_button.image.color.a > 0.9 && !_finished)
